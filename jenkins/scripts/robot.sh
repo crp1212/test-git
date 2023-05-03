72e1a9d2479006071e35e1e28f7dae7e0a2f4b99
crp1212 <403454318@qq.com>
2023-05-03 08:29:20 +0800

    更新JenkinsFile1683073760363

--- jenkins/scripts/robot.sh
+++ jenkins/scripts/robot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=4848de26-980a-4c3e-8d89-1d8d2e8897b2' \
+/usr/bin/curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=4848de26-980a-4c3e-8d89-1d8d2e8897b2' \
    -H 'Content-Type: application/json' \
    -d '
    {
